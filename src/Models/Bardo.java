54d54dc3ea26d69cf6ec9555d17ea5cb76c40f45
Vinícius Madureira Godinho de Oliveira <vinimagod@gmail.com>
2025-03-19 21:11:36 -0300

    o evaristo é mto chato

--- src/Models/Bardo.java
+++ src/Models/Bardo.java
@@ -1,5 +1,27 @@
-package Models;
-
-public class Bardo {
+import java.util.ArrayList;
+public class Bardo extends Personagem{
     
+    private int inspiracao;
+    private ArrayList<Magia> magia = new ArrayList<>();
+
+    public Bardo(String nome, int forca, int vida, int inspiracao, ArrayList<Magia> magia){
+        super(nome,forca,vida);
+        this.inspiracao = inspiracao;
+        this.magia = magia;
+    }
+
+    public int getInspiracao() {
+        return inspiracao;
+    }
+
+    public void setInspiracao(int inspiracao) {
+        this.inspiracao = inspiracao;
+    }
+
+    
+
+    @Override
+    public String toString(){
+        return super.toString() + "\ninspiração: " + inspiracao;
+    }
 }
